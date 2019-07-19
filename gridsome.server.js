50c2b10473538a41c7ea688cc7f81af100fde39f
Jozef Maxted <jozef.maxted@stratumfive.com>
2019-07-19 14:41:03 +0100

    Trigger reload

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,11 +6,27 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
+  // console.log(api);
   api.loadSource(({ addContentType }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
+    console.log('Loadddddddd')
   })
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api
+  })
+
+  api.configureServer(app => {
+    app.get('/my-endpoint', (req, res) => {
+      console.log(api._app.store.store.collections);
+      api._app.store.store.collections
+        .filter((collection) => !collection.name.includes('core'))
+        .forEach((collection) => console.log(collection.find()));
+      res.send('Hello, world!')
+      // api._app.init();
+      setTimeout(() => {
+        api._app.bootstrap();
+      },2000);
+    })
   })
 }
